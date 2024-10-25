fdd49f4c80eb6603e66aaf2eca6ab8d25e44b7f0
2412pooja1 <pdalvi0203@gmail.com>
2024-10-25 16:41:51 +0530

    changes

--- Models/carrousel.js
+++ Models/carrousel.js
@@ -8,10 +8,6 @@ const Carrousal = sequelize.define(
       type: DataTypes.STRING,
       allowNull: true,
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: true, // You can set this to `false` if the title is required
-    },
     isActive: {
       type: DataTypes.BOOLEAN,
       defaultValue: false,

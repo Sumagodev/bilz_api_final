697dd1219b2fb2c7bd640603c078c8fc18777e62
2412pooja1 <pdalvi0203@gmail.com>
2024-10-28 15:24:15 +0530

    changes

--- Routes/contactPersonRoutes.js
+++ Routes/contactPersonRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { uploadFiles } = require('../middleware/fileUploadMiddleware');
+const { upload } = require('../middleware/multer');
+
 const { validateContactPerson, validateContactPersonId } = require('../Validations/contactPersonValidation');
 const {
   addContactPerson,
@@ -12,8 +14,11 @@ const authenticateToken = require('../middleware/auth');
 const imageRequired = require('../Validations/imageValidation');
 const router = express.Router();
 
-router.post('/add', uploadFiles, imageRequired, validateContactPerson, addContactPerson);
-router.put('/update/:id', uploadFiles, validateContactPerson, validateContactPersonId, updateContactPerson);
+
+router.post('/add', upload.single('img'), authenticateToken, addContactPerson);
+router.put('/update/:id', upload.single('img'), authenticateToken, validateContactPersonId, updateContactPerson);
+
+
 router.get('/get', getContactPersons);
 router.get('/find', authenticateToken, getContactPersons);
 router.put('/isactive/:id', authenticateToken, validateContactPersonId, toggleContactPersonStatus);
